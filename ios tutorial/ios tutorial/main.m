2152caff100a73a03ffe9f96f634731a5da5b60c
Mirmillo <mirmillo@gmail.com>
2012-09-16 13:46:40 -0400

    reset author

---
+++ ios tutorial/ios tutorial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ios tutorial
+//
+//  Created by David Damstra on 9/16/12.
+//  Copyright (c) 2012 David Damstra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "mirmilloAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([mirmilloAppDelegate class]));
+    }
+}
---
+++ ios tutorial/ios tutorialTests/ios_tutorialTests.m
@@ -0,0 +1,32 @@
+//
+//  ios_tutorialTests.m
+//  ios tutorialTests
+//
+//  Created by David Damstra on 9/16/12.
+//  Copyright (c) 2012 David Damstra. All rights reserved.
+//
+
+#import "ios_tutorialTests.h"
+
+@implementation ios_tutorialTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ios tutorialTests");
+}
+
+@end
